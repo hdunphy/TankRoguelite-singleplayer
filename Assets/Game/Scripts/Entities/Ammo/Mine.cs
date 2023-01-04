568c5579d5b86625d29b35000ef220c41c3ee831
Henry Dunphy <hdunphy15@gmail.com>
2023-01-04 13:44:20 -0500

    Added Mine

---
+++ Assets/Game/Scripts/Entities/Ammo/Mine.cs
@@ -0,0 +1,112 @@
+﻿using Assets.Game.Scripts.Entities.Interfaces;
+using Assets.Game.Scripts.Entities.ScriptableObjects;
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Game.Scripts.Entities.Ammo
+{
+    [RequireComponent(typeof(Collider2D), typeof(Damagable))]
+    public class Mine : MonoBehaviour, IAmmo
+    {
+        [SerializeField] private GameObject explosion;
+        [SerializeField] private SpriteRenderer spriteRenderer;
+
+        [SerializeField] private Color flashColor;
+        [SerializeField] private float flashTime;
+
+        private MineData _data;
+        private float _detonationTime;
+        private float _timerDelay;
+        private bool _isDetonating = false;
+
+        public event Action OnDestroyed;
+
+        public void Initialize(AmmoData data)
+        {
+            _data = data as MineData;
+            _detonationTime = _data.TimerDelay + Time.time;
+            _timerDelay = _data.TimerDelay;
+
+            StartCoroutine(Flash());
+        }
+
+        private void OnDestroy()
+        {
+            OnDestroyed?.Invoke();
+        }
+
+        private void Update()
+        {
+            if (Time.time >= _detonationTime)
+            {
+                StartCoroutine(Explode());
+            }
+        }
+
+        private IEnumerator Flash()
+        {
+            float interval = (_timerDelay - (4 * flashTime)) / 4;
+            Color startColor = spriteRenderer.color;
+
+            for (int i = 0; i < 3; i++)
+            {
+                yield return new WaitForSeconds(interval);
+                spriteRenderer.color = flashColor;
+                yield return new WaitForSeconds(flashTime);
+                spriteRenderer.color = startColor;
+            }
+
+            while (spriteRenderer.enabled)
+            {
+                yield return new WaitForSeconds(flashTime);
+                spriteRenderer.color = flashColor;
+                yield return new WaitForSeconds(flashTime);
+                spriteRenderer.color = startColor;
+            }
+        }
+
+        private IEnumerator Explode()
+        {
+            _isDetonating = true;
+            ExplodeParticleEffect();
+
+            DealDamage(_data.ExplosionInnerRadius);
+
+            yield return new WaitForSeconds(_data.ExplosionOuterDelay);
+
+            DealDamage(_data.ExplosionRadius);
+
+            yield return new WaitForSeconds(_data.OnDestroyDelay);
+
+            Destroy(gameObject);
+        }
+
+        private void DealDamage(float _radius)
+        {
+            var colliderInRadius = Physics2D.OverlapCircleAll(transform.position, _radius);
+
+            foreach (Collider2D _collider in colliderInRadius)
+            {
+                if (_collider.TryGetComponent(out Damagable damageable) && _collider.gameObject != gameObject)
+                {
+                    damageable.TakeDamage(_data.Damage, _data.DamageType);
+                }
+            }
+        }
+
+        private void ExplodeParticleEffect()
+        {
+            explosion.SetActive(true);
+            spriteRenderer.enabled = false;
+        }
+
+        public void InitiateExposion()
+        {
+            if (!_isDetonating)
+            {
+                StartCoroutine(Explode());
+            }
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Entities/ScriptableObjects/MineData.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Assets.Game.Scripts.Entities.ScriptableObjects
+{
+    [CreateAssetMenu(menuName = "Data/Ammo/Mine")]
+    public class MineData : AmmoData
+    {
+        [SerializeField] private float timerDelay = 1;
+        [SerializeField] private float explosionRadius;
+        [SerializeField] private float explosionInnerRadius;
+        [SerializeField] private float explosionOuterDelay = .25f;
+        [SerializeField] private float onDestroyDelay = .5f;
+        [SerializeField] private int health = 1;
+
+        public float TimerDelay => timerDelay;
+        public float ExplosionRadius => explosionRadius;
+        public float ExplosionInnerRadius => explosionInnerRadius;
+        public float ExplosionOuterDelay => explosionOuterDelay;
+        public float OnDestroyDelay => onDestroyDelay;
+        public int Health => health;
+
+        public override bool CheckShot(Vector2 from, Vector2 to, int currentNumberOfBounces, float distance, ref Vector2 targetPoint, LayerMask layerMask, bool debug = false)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
