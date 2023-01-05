026dd65ff6457b0bb671263b6a7a88c816605c26
Henry Dunphy <hdunphy15@gmail.com>
2023-01-04 20:25:21 -0500

    Added pickups

---
+++ Assets/Game/Scripts/Entities/Pickups/AbilityPickup.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Game.Scripts.Controllers;
+using Assets.Game.Scripts.Entities.Abilities;
+using UnityEngine;
+
+namespace Assets.Game.Scripts.Entities.Pickups
+{
+    public class AbilityPickup : CollectablePickup
+    {
+        /*Component attached to an ingame ability pickup*/
+
+        [SerializeField] private Ability ability; //Ability given to player upon pickup
+        [SerializeField] private SpriteRenderer spriteRenderer;
+        [SerializeField] private bool isPrimaryAbility;
+
+        private void Start()
+        {
+            spriteRenderer.sprite = ability.Sprite;
+        }
+
+        public override void OnPickup(PlayerController controller)
+        {
+            controller.AddAbility(ability, isPrimaryAbility);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Entities/Pickups/CollectablePickup.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Game.Scripts.Controllers;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.Game.Scripts.Entities.Pickups
+{
+    [RequireComponent(typeof(BoxCollider2D))]
+    public abstract class CollectablePickup : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent OnPickupEvent; //Triggers on a successful pickup
+
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.TryGetComponent(out PlayerController controller))
+            {
+                OnPickup(controller);
+            }
+        }
+
+        public abstract void OnPickup(PlayerController controller);
+    }
+}
