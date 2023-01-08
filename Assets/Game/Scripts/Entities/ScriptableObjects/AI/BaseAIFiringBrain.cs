0da2163d6c4afe808288450a23e0c6785b0748b1
Henry Dunphy <hdunphy15@gmail.com>
2023-01-08 18:16:24 -0500

    Updated enemy ai

---
+++ Assets/Game/Scripts/Entities/ScriptableObjects/AI/BaseAIFiringBrain.cs
@@ -0,0 +1,37 @@
+﻿using Assets.Game.Scripts.Controllers;
+using Assets.Game.Scripts.Entities.Abilities;
+using UnityEngine;
+
+namespace Assets.Game.Scripts.Entities.ScriptableObjects.AI
+{
+    public abstract class BaseAIFiringBrain : ScriptableObject
+    {
+
+        protected TankGunMovement _tankGunMovement;
+        protected FiringController _firingController;
+        protected Transform _selfTransform;
+
+        public virtual void Initialize(BaseAIFiringBrainInput input)
+        {
+            _tankGunMovement = input.TankGunMovement;
+            _firingController = input.FiringController;
+            _selfTransform = input.SelfTransform;
+        }
+
+        public abstract void Update(float deltaTime);
+
+    }
+    public class BaseAIFiringBrainInput
+    {
+        public TankGunMovement TankGunMovement { get; set; }
+        public FiringController FiringController { get; set; }
+        public Transform SelfTransform { get; set; }
+
+        public BaseAIFiringBrainInput(TankGunMovement tankGunMovement, FiringController firingController, Transform selfTransform)
+        {
+            TankGunMovement = tankGunMovement;
+            FiringController = firingController;
+            SelfTransform = selfTransform;
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Entities/ScriptableObjects/AI/RandomAIFiringBrain.cs
@@ -0,0 +1,58 @@
+﻿using Assets.Game.Scripts.Controllers;
+using Assets.Game.Scripts.Entities.Abilities;
+using UnityEngine;
+
+namespace Assets.Game.Scripts.Entities.ScriptableObjects.AI
+{
+    public class RandomAIFiringBrainInput : BaseAIFiringBrainInput
+    {
+        public RandomAIFiringBrainInput(TankGunMovement tankGunMovement, FiringController firingController, Transform selfTransform) : base(tankGunMovement, firingController, selfTransform)
+        {
+        }
+    }
+
+    [CreateAssetMenu(menuName = "Data/AI/Random Firing Brain")]
+    public class RandomAIFiringBrain : BaseAIFiringBrain
+    {
+        [SerializeField] private float timeBetweenMoves = 0.5f;
+        [SerializeField] private AmmoData ammoData;
+        [SerializeField] private LayerMask badTargets;
+
+        private bool hasNextShot = false;
+        private float _remainingTime = 0f;
+
+        private Vector2 _lookDirection;
+
+        public override void Initialize(BaseAIFiringBrainInput input)
+        {
+            base.Initialize(input);
+
+            FindNextLookPoint();
+        }
+
+        public override void Update(float deltaTime)
+        {
+            _remainingTime -= deltaTime;
+            if (hasNextShot && _remainingTime < 0)
+            {
+                hasNextShot = false;
+                FindNextLookPoint();
+            }
+            else if (_tankGunMovement.IsAligned && !hasNextShot)
+            {
+                _remainingTime = timeBetweenMoves;
+                hasNextShot = true;
+            }
+
+            Vector2 targetVector = Vector2.one;
+            var hitsAlly = ammoData.CheckShot(_selfTransform.position, _lookDirection, 0, 100, ref targetVector, badTargets);
+            _firingController.SetAbilityButtonPressed(!hitsAlly);
+        }
+
+        private void FindNextLookPoint()
+        {
+            _lookDirection = Random.insideUnitCircle;
+            _tankGunMovement.SetLookPoint(_lookDirection);
+        }
+    }
+}
